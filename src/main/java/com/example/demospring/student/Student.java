e9996f46895b15190a7c87e992f2d6df5b245862
Joseph A <joseph.atureta@gmail.com>
2021-06-06 23:41:14 +0100

    Student class using derby

--- src/main/java/com/example/demospring/student/Student.java
+++ src/main/java/com/example/demospring/student/Student.java
@@ -1,22 +1,13 @@
 package com.example.demospring.student;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Table
 public class Student {
     @Id
-    @SequenceGenerator(
-        name = "student_sequence",
-        sequenceName = "student_sequence",
-        allocationSize = 1
-    )
-    @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator =  "student_sequence"
-    )
     private Long id;
     private String name;
     private String email;
